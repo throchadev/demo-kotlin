bbe5e4845b52e409e9685fa2323e9cf784bdca44
thiagorocha <thiago.rocha@soulisto.com.br>
2022-06-28 17:57:28 -0300

    first commit

---
+++ src/main/kotlin/br/com/throchadev/demokotlin/DemoKotlinApplication.kt
@@ -0,0 +1,10 @@
+package br.com.throchadev.demokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoKotlinApplication
+
+fun main(args: Array<String>) {
+	println("Hello World!")
+}
